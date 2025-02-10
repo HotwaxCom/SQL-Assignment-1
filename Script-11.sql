ca83e62c8feac4592a2b858582c12dc761613ee7
Shreya Singh <shreya.xa02@gmail.com>
2025-02-10 19:27:53 +0530

    changes to script-11 done

--- Script-11.sql
+++ Script-11.sql
@@ -4,5 +4,7 @@ select
 from order_status os 
 	where os.status_id="ORDER_CANCELLED" 
 	and os.status_datetime between '2025-01-01' and '2025-01-31' order by status_datetime;
+	
+/*added a date filter*/
 
 
