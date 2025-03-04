9164a44fdca03aa81a29147d2503b117088614dd
Shreya Singh <104385986+Shreyyx@users.noreply.github.com>
2025-03-04 17:02:37 +0530

    Delete Script-9.sql

--- Script-9.sql
+++
@@ -1,9 +0,0 @@
-Select 
-    hour(created_stamp) AS order_hour,
-    count(order_id) AS orders_completed
-from order_header
-where status_id = 'ORDER_COMPLETED'
-group by order_hour
-order by order_hour;
-
-
