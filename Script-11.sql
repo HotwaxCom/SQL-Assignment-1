a8f4f2888ca03a4e02611f8267a40f9c03ae5581
Shreya Singh <104385986+Shreyyx@users.noreply.github.com>
2025-03-04 17:03:05 +0530

    Delete Script-11.sql

--- Script-11.sql
+++
@@ -1,8 +0,0 @@
-select
-	count(os.order_id),
-	os.change_reason as cancellation_reason
-from order_status os 
-	where os.status_id="ORDER_CANCELLED" 
-	and os.status_datetime between '2025-01-01' and '2025-01-31' order by status_datetime;
-
-
